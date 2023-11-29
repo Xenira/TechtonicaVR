13f32317e24a33c5cf9a40ee13467601ed5bee22
Xenira <1288524+Xenira@users.noreply.github.com>
2023-11-29 22:14:27 +0100

    fix(render): disable outline shader draw
    The shader does not work in vr. Disabled for now.

--- plugin/src/patch/universal/disable_component_patch.cs
+++ plugin/src/patch/universal/disable_component_patch.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 namespace TechtonicaVR.Patches.Universal;
 
-public class DisableOnComponentPatch<T> : GameComponentPatch<T> where T : MonoBehaviour
+public class DisableComponentPatch<T> : GameComponentPatch<T> where T : MonoBehaviour
 {
     private bool applied = false;
 
     protected override bool Apply(T component)
     {
-        component.gameObject.SetActive(false);
+        component.enabled = false;
         return true;
     }
 }
