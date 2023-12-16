2653c8fd1fc27169f290e6ff7f440021655f2e6a
Xenira <1288524+Xenira@users.noreply.github.com>
2023-12-16 03:01:04 +0100

    feat(ui): attched inspector to aim location

---
+++ plugin/src/camera/patch/builder/player_inspector_patch.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Plugin.Input;
+using UnityEngine;
+
+namespace TechtonicaVR.VRCamera.Patch.Builder;
+
+[HarmonyPatch]
+public class PlayerInspectorPatch
+{
+	private const int EXPECTED_INSPECTOR_UPDATE_PATCH_COUNT = 1;
+
+	static MethodInfo raycastMethod = typeof(Physics).GetMethod(nameof(Physics.Raycast), [typeof(Vector3), typeof(Vector3), typeof(RaycastHit).MakeByRefType(), typeof(float), typeof(int)]);
+	static MethodInfo raycastPatchMethod = typeof(PlayerInspectorPatch).GetMethod(nameof(PatchedRaycast));
+
+	[HarmonyPatch(typeof(PlayerInspector), nameof(PlayerInspector.LateUpdate))]
+	[HarmonyTranspiler]
+	public static IEnumerable<CodeInstruction> InspectorUpdateTranspiler(IEnumerable<CodeInstruction> instructions)
+	{
+		var patchCnt = 0;
+		foreach (var instruction in instructions)
+		{
+			if (instruction.Calls(raycastMethod))
+			{
+				yield return new CodeInstruction(OpCodes.Call, raycastPatchMethod);
+				patchCnt++;
+			}
+			else
+			{
+				yield return instruction;
+			}
+		}
+
+		if (patchCnt != EXPECTED_INSPECTOR_UPDATE_PATCH_COUNT)
+		{
+			Plugin.Logger.LogError($"[PlayerInspector.LateUpdate] Patch count mismatch: {patchCnt} != {EXPECTED_INSPECTOR_UPDATE_PATCH_COUNT}");
+		}
+	}
+
+	public static bool PatchedRaycast(Vector3 _origin, Vector3 _direction, out RaycastHit hitInfo, float maxDistance, int layerMask)
+	{
+		return Physics.Raycast(SteamVRInputMapper.rightHandObject.transform.position, -SteamVRInputMapper.rightHandObject.transform.up, out hitInfo, maxDistance, layerMask);
+	}
+
+}
