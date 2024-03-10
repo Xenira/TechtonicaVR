57872346a815b4dff34ade012ccecf156e97cbe8
Xenira <1288524+Xenira@users.noreply.github.com>
2024-03-10 18:24:03 +0100

    fix(input): change vertical belt targeting to use hand (#136)

--- plugin/src/camera/patch/builder/conveyor_builder_patch.cs
+++ plugin/src/camera/patch/builder/conveyor_builder_patch.cs
@@ -9,8 +9,15 @@ class ConveyorBuilderPatch
 {
 	[HarmonyPatch(typeof(ConveyorBuilder), nameof(ConveyorBuilder.GetTargetEndPt))]
 	[HarmonyTranspiler]
-	public static IEnumerable<CodeInstruction> ReassembleTranspiler(IEnumerable<CodeInstruction> instructions)
+	public static IEnumerable<CodeInstruction> GetTargetEndPtReassemble(IEnumerable<CodeInstruction> instructions)
 	{
-		return Transpile.reassemblePatch<ConveyorBuilder>(instructions, 5);
+		return Transpile.reassemblePatch<ConveyorBuilder>(instructions, 3);
+	}
+
+	[HarmonyPatch(typeof(ConveyorBuilder), nameof(ConveyorBuilder.UpdateEndForVerticalBelt))]
+	[HarmonyTranspiler]
+	public static IEnumerable<CodeInstruction> UpdateEndForVerticalBeltReassemble(IEnumerable<CodeInstruction> instructions)
+	{
+		return Transpile.reassemblePatch<ConveyorBuilder>(instructions, 2);
 	}
 }
