81665bb80def170a9deb36cb2e819785849d5e07
Xenira <1288524+Xenira@users.noreply.github.com>
2024-03-10 10:13:34 +0100

    fix(coop): sync player rotation with headset

---
+++ plugin/src/camera/patch/PlayerFirstPersonControllerPatch.cs
@@ -0,0 +1,27 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace TechtonicaVR.VRCamera.Patch;
+
+[HarmonyPatch]
+class PlayerFirstPersonControllerPatch
+{
+	[HarmonyPatch(typeof(PlayerFirstPersonController), nameof(PlayerFirstPersonController.AutoCrouch))]
+	[HarmonyPrefix]
+	static bool AutoCrouchPatch()
+	{
+		return true;
+	}
+
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(PlayerFirstPersonController), nameof(PlayerFirstPersonController.UpdateThirdPersonView))]
+	public static bool UpdateThirdPersonView()
+	{
+		if (Player.instance.networkedPlayer)
+		{
+			Player.instance.networkedPlayer.display.transform.position = Player.instance.transform.position;
+			Player.instance.networkedPlayer.display.transform.rotation = Quaternion.Euler(0f, VRCameraManager.mainCamera.transform.rotation.eulerAngles.y, 0f);
+		}
+		return false;
+	}
+}
